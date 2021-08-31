a73c78c2db7d6144913b2d7f75d0d491bb52d4f2
Yogesh Dubey <yogesh.dubey@engageasap.com>
2021-08-31 13:33:41 +0530

    minor change

--- packages/new_project/src/App.js
+++ packages/new_project/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import myFirstApi from "@yogesh/my-first-api";
+import reTestPackage from "@yogesh/re_test_package";
 
 function App() {
-  return myFirstApi()
+  return (
+    reTestPackage()
+  )
 }
 
 export default App;
